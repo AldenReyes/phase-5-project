f0611120df3829c7680c34aaa73cdc83dc3176bb
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2023-10-10 11:22:31 -0700

    Adds Marshmallow schemas, 4 main get all routes

--- server/app.py
+++ server/app.py
@@ -1,24 +1,100 @@
 #!/usr/bin/env python3
-
-# Standard library imports
-
-# Remote library imports
-from flask import request
+from flask import Flask, request, make_response
 from flask_restful import Resource
-
-# Local imports
-from config import app, db, api, ma
-
-# Add your model imports
+from flask_restful import Api, Resource
+from config import app, ma, api
+from models import User, DreamLog, Tag, DreamTag
 
 
-# Views go here!
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
+
+    id = ma.auto_field()
+    username = ma.auto_field()
 
 
-@app.route('/')
-def index():
-    return '<h1>Project Server</h1>'
+user_singular_schema = UserSchema()
+user_plural_schema = UserSchema(many=True)
 
+
+class Users(Resource):
+    def get(self):
+        users = User.query.all()
+        response = make_response(user_plural_schema.dump(users), 200)
+        return response
+
+
+class DreamLogSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = DreamLog
+
+    id = ma.auto_field()
+    title = ma.auto_field()
+    text_content = ma.auto_field()
+    is_public = ma.auto_field()
+    rating = ma.auto_field()
+
+    published_at = ma.auto_field()
+    edited_at = ma.auto_field()
+
+    user_id = ma.auto_field()
+
+
+dream_log_singular_schema = DreamLogSchema()
+dream_log_plural_schema = DreamLogSchema(many=True)
+
+
+class DreamLogs(Resource):
+    def get(self):
+        dream_logs = DreamLog.query.all()
+        response = make_response(dream_log_plural_schema.dump(dream_logs), 200)
+        return response
+
+
+class TagSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Tag
+
+    id = ma.auto_field()
+    name = ma.auto_field()
+
+
+tag_singular_schema = TagSchema()
+tag_plural_schema = TagSchema(many=True)
+
+
+class Tags(Resource):
+    def get(self):
+        tags = Tag.query.all()
+        response = make_response(tag_plural_schema.dump(tags))
+        return response
+
+
+class DreamTagSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = DreamTag
+
+    id = ma.auto_field()
+    dream_log_id = ma.auto_field()
+    tag_id = ma.auto_field()
+
+
+dream_tag_singular_schema = DreamTagSchema()
+dream_tag_plural_schema = DreamTagSchema(many=True)
+
+
+class DreamTags(Resource):
+    def get(self):
+        dream_tags = DreamTag.query.all()
+        response = make_response(dream_tag_plural_schema.dump(dream_tags))
+        return response
+
+
+api.add_resource(Users, '/users')
+api.add_resource(DreamLogs, '/dream-logs')
+api.add_resource(Tags, '/tags')
+api.add_resource(DreamTags, '/dream-tags')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
