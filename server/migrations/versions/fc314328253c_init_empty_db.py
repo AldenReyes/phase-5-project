b73e01d0d0d5ea5674b45c3a99ff1e9f8e1cc538
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2023-10-10 08:17:57 -0700

    Init empty db, adds db to gitignore

---
+++ server/migrations/versions/fc314328253c_init_empty_db.py
@@ -0,0 +1,24 @@
+"""init empty db
+
+Revision ID: fc314328253c
+Revises: 
+Create Date: 2023-10-10 08:16:10.316985
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fc314328253c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
