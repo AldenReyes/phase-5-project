d3c49d82bdb4cc25dc5f274bf9c0cc55d3f19318
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2023-10-11 03:14:04 -0700

    Initial front end structure / components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,40 @@
 import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import DreamLogs from "./components/DreamLogs";
+import PersonalLogs from "./components/PersonalLogs";
+import CreateLog from "./components/CreateLog";
 
 function App() {
-  return <h1>Project Client</h1>;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Check if user is logged in here
+    // Set isLoggedIn accordingly
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar isLoggedIn={isLoggedIn} />
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/dreamlogs" component={DreamLogs} />
+          {isLoggedIn && (
+            <>
+              <Route path="/personallogs" component={PersonalLogs} />
+              <Route path="/create" component={CreateLog} />
+            </>
+          )}
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./components/App";
-import "./index.css";
-import { createRoot } from "react-dom/client";
+import "./styles/index.css";
 import "semantic-ui-css/semantic.min.css";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
