81256edf38eda4b9ee8ba077f6af687040fba19d
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2023-10-11 04:00:39 -0700

    Adds components with placeholder elements

---
+++ client/src/components/DreamLogs.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DreamLogs() {
+  return <h1>Dream Logs</h1>;
+}
---
+++ client/src/components/PersonalLogs.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PersonalLogs() {
+  return <h1>PersonalLogs</h1>;
+}
