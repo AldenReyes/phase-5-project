97854bdd859f9f5c3f8559b58b6c0c60c7f78934
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2023-10-11 05:10:43 -0700

    Changes buttons to fix React console error

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,19 +1,32 @@
 import React from "react";
-import { Button } from "semantic-ui-react";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import { Image } from "semantic-ui-react";
 import "../styles/Home.css";
 
 export default function Home() {
+  const history = useHistory();
+
+  const navigateToPath = (path) => {
+    history.push(path);
+  };
+
   return (
     <div className="home">
-      <h1>Welcome to Dream Logs</h1>
+      <Image src="background.JPG" size="large" bordered />
+      <h1>Welcome to Dream Logger</h1>
       <div>
-        <Link to="/login">
-          <Button primary>Login</Button>
-        </Link>
-        <Link to="/signup">
-          <Button secondary>Sign Up</Button>
-        </Link>
+        <button
+          className="ui primary button"
+          onClick={() => navigateToPath("/login")}
+        >
+          Login
+        </button>
+        <button
+          className="ui secondary button"
+          onClick={() => navigateToPath("/signup")}
+        >
+          Sign Up
+        </button>
       </div>
     </div>
   );
